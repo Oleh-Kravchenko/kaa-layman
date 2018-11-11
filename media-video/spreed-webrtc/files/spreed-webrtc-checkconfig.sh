08eae8088e66c015d46454a0badcaf1d5d86723a
Oleh Kravchenko <oleg@kaa.org.ua>
2018-11-11 20:45:39 +0200

    Drop outdated ebuilds
    dev-libs/dyncall
    dev-libs/libcpuid
    dev-python/bluepy
    dev-python/btce-api
    dev-util/codelite
    dev-util/jenkins-bin
    games-arcade/mininim
    media-video/spreed-webrtc
    net-libs/cppzmq
    net-misc/cpp-ethereum
    sys-auth/pam_encfs

--- media-video/spreed-webrtc/files/spreed-webrtc-checkconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-WEBRTC_CONF="${WEBRTC_CONF:-/etc/spreed-webrtc/spreed-webrtc.ini}"
-
-if [ ! -e ${WEBRTC_CONF} ] ; then
-	echo "Please setup ${WEBRTC_CONF} before starting spreed-webrtc" >&2
-	echo "There are sample configurations in /etc/spreed-webrtc" >&2
-	exit 1
-fi
